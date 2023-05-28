355f3c49a22ab57969ff828e6cc04e2610fff048
Alexandre Medeiros <alexandre.koshy@gmail.com>
2023-05-28 16:47:29 -0300

    get location

--- index.js
+++ index.js
@@ -3,13 +3,22 @@ const listRideElement = document.querySelector('#rideList')
 const allRides = getAllRides()
 console.log(allRides)
 
-allRides.forEach(([id, value]) => {
+allRides.forEach(async ([id, value]) => {
     const ride = JSON.parse(value)
     ride.id = id
 
+    const firstPosition = ride.data[0]
+    const firstLocationData =  await getLocationData(firstPosition.latitude, firstPosition.longitude);
+
     const itemElement = document.createElement('li');
-    itemElement.innerText = ride.id;
+    itemElement.innerText = `${firstLocationData.city} - ${firstLocationData.countryCode}`
     itemElement.id = ride.id; //adidiona o id como ride.id
 
     listRideElement.appendChild(itemElement)
 });
+
+async function getLocationData(latitude, longitude) {
+    const url = `http://api.bigdatacloud.net/data/reverse-geocode-client?latitude=${latitude}&longitude=${longitude}&=localityLanguage=en`
+    const response = await fetch(url)
+    return await response.json()
+}
